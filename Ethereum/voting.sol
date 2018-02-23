c12816d6048869726dffaa063edd74aa95e15de4
Chrysostomos Daniel <daniel.chrysostomos@gmail.com>
2018-02-23 22:29:28 +0200

    Voting get improvment details function

--- Ethereum/voting.sol
+++ Ethereum/voting.sol
@@ -20,4 +20,18 @@ contract Voting {
         improvmentsStructs[referenceNo].total_votes = 0;
         improvmentsStructs[referenceNo].description = description;
     }
+
+    function getImprovmentsInformation(bytes32 referenceNo) public constant returns(bytes32 title,uint8 total_yes, uint8 total_no, uint8 total_votes, bytes32 description) {
+         for(uint i = 0; i < improvmentList.length; i++) {
+            if (improvmentList[i] == referenceNo) {
+                return(
+                improvmentsStructs[referenceNo].title,
+                improvmentsStructs[referenceNo].total_yes,
+                improvmentsStructs[referenceNo].total_no,
+                improvmentsStructs[referenceNo].total_votes,
+                improvmentsStructs[referenceNo].description
+                );
+            }
+        }
+    }
 }
